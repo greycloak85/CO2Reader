8d7b781f836d16e2ff4f82b648cfc840d29383f4
greycloak85 <william.lefew@gmail.com>
2015-09-20 14:53:10 -0400

    small update with questions

--- Survey Sandbox/SandBox.R
+++ Survey Sandbox/SandBox.R
@@ -15,3 +15,19 @@ respondents=200;
 surveyResults=matrix(sample.int(max(answerVals), size = questions*respondents, replace = TRUE), nrow = questions, ncol = respondents)
 
 
+#*********Below are questions to answer***************************
+
+
+#Are the responses random?  (i.e. not spoofed, honestly generated, is there structure)
+
+
+
+#What is the maximal group of questions such the N consistent responses are produced?
+
+
+
+#What are the 2 groups of questions such the N_1+N_2>=N consistent responses are produced?
+
+
+
+#What are the M groups of questions such that sum(N_i, 1, M)>=N consistent responses are produced?
