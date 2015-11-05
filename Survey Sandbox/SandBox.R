2f4592ed3a927d072713e013a72d340c5a39a7e1
Rich Yaxley <richardyaxley@gmail.com>
2015-11-04 20:49:24 -0500

    Added some more about hclust()

--- Survey Sandbox/SandBox.R
+++ Survey Sandbox/SandBox.R
@@ -5,7 +5,7 @@
 # about opportunities, product definition, and positioning, and can engage in 
 # more effective promotion. 
 
-# Last Updated on October 28, 2015
+# Last Updated on November 4, 2015
 
 rm(list = ls(all = TRUE))  # Equivalent to "Clear All" in Matlab
 
@@ -17,17 +17,17 @@ syntactic.device <- Sys.info()["nodename"]
 if(syntactic.device == 'Richs-MacBook-Air.local'){
   setwd('/Users/Rich_Yaxley/Dropbox (Personal)/Work/Git/CO2Reader/Survey Sandbox')
 } else if (syntactic.device == 'JIMS_MACHINE'){
-  setwd('C:\Users\Jim and Kristy\Dropbox\???')
+  setwd('C:/JIMPATH')
 } else if (syntactic.device == 'BILLS_MACHINE'){
-  setwd('C:\Users\Greycloak')
+  setwd('C:/BILLPATH')
 }
 
-dir() # List contents to make sure you are in the righe directory
+dir() # List contents to make sure you are in the right directory
 
 # Define the variable types associated with the survey
 maxVal = 5 # Largest value for Likert scale question
 numQuest = 15 # A column for each question
-numResp = 500 # A row for each respondent
+numResp = 100 # A row for each respondent
 
 # Generate random answers to each question for each respondent
 set.seed(18)
@@ -37,6 +37,11 @@ df <- as.data.frame(m)
 id <- sample(c('0':'9999999'), numResp, replace=F)
 age <- sample(18:65, numResp, replace=TRUE)
 sex <- sample(c('M','F'), numResp, replace=T)
+
+# LOAD DATA
+# open survey data from Habitat for Humanity study
+# clean data 
+# 
 
 # Combine columns, and question matrix
 df <- cbind(id, age, sex, df)
@@ -59,11 +64,33 @@ head(df)
 # hclust uses distance measure
 require(cluster)
 
-seg.dist <- daisy(df)
-seg.hc <- hclust(seg.dist, method='complete')
+seg.dist <- daisy(df) # distance function
+as.matrix(seg.dist)[1:7,1:7]
+seg.hc <- hclust(seg.dist, method='complete') 
 plot(seg.hc)
 # Zoom in
-plot(cut(as.dendrogram(seg.hc), h=0.35)$lower[[1]])
+plot(cut(as.dendrogram(seg.hc), h=9)$lower[[1]])
+
+# Check the similarity of a few pairs
+df[c(45, 77), ] # similar
+df[c(1, 31), ] # similar
+df[c(45, 31), ] # dissimilar
+
+# Check goodness-of-fit with the Cophenetic correlation coefficient (CPCC)
+cophenetic(seg.hc)
+cor(cophenetic(seg.hc), seg.dist)
+
+# Determine how many groups we want for the segmentation
+plot(seg.hc)
+# Cut at 4 groups
+rect.hclust(seg.hc, k=4, border='red')
+seg.hc.segment <- cutree(seg.hc, k=4) # Vector with group membership labels
+table(seg.hc.segment) # Table with size of segments
+
+# plot(jitter(as.numeric(df$V1)) ~ 
+#      jitter(as.numeric(df$V2)),
+#      col=seg.hc.segment, yaxt='n', xact='n', ylab='', xlab='')
+
 
 
 # Mean-based Clustering: kmeans()
